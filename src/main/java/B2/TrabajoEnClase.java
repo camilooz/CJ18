a2e2722563fdd767f156f7f8715140b140e07b57
camilooz <crisk270697@gmail.com>
2023-10-07 14:44:21 -0500

    Trabajo en clase

---
+++ src/main/java/B2/TrabajoEnClase.java
@@ -0,0 +1,21 @@
+package B2;
+
+import java.util.Scanner;
+
+public class TrabajoEnClase {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite la cantidad de dolares a convertir");
+        int valor = scanner.nextInt();
+
+        conver(valor,4386);
+
+    }
+
+    private static void conver(int valor, int i) {
+        System.out.println("es valor es: "+(valor*i));
+    }
+}
